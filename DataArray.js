d36cdeec5a31698f5442cf1b9a91f0afb8cf66dc
patel-rupin2000 <62296986+patel-rupin2000@users.noreply.github.com>
2020-04-12 21:17:11 +0530

    Add files via upload

---
+++ DataArray.js
@@ -0,0 +1,77 @@
+export const meals=[
+    {
+        id:1,
+        name: 'grilled sandwhiches',
+        photo_url:
+        'https://recipes.timesofindia.com/thumb/54714340.cms?imgsize=458099&width=509&height=340'
+    },
+    {
+        id:2,
+        name:'coffee',
+        photo_url:
+        'https://i0.wp.com/cdn-prod.medicalnewstoday.com/content/images/articles/323/323594/white-cup-with-black-coffee.jpg?w=1155&h=1541'
+    },
+    {
+        id:3,
+        name:'burgers',
+        photo_url:
+        'https://www.seriouseats.com/recipes/images/2015/07/20150702-sous-vide-hamburger-anova-primary-1500x1125.jpg'
+    },
+    {
+        id:4,
+        name:'cookies',
+        photo_url:
+        'https://www.texanerin.com/content/uploads/2018/06/no-bake-lactation-cookies-1-650x975.jpg'
+    },
+    {
+        id:5,
+        name:'smoothies',
+        photo_url:
+        'https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2018/08/Strawberry-Banana-Smoothie-12.jpg'
+    }
+];
+export const recipes=[
+    {
+        recipeId:1,
+        categoryId:1,
+        title:'veg cheese sandwhich',
+        photo_url:
+        'https://www.paneraathome.com/-/media/banners/mac-and-cheese-grilled-cheese.jpg'
+    },
+    {
+        recipeId:2,
+        categoryId:1,
+        title:'egg sandwhich',
+        photo_url:
+        'https://cdn3.tmbi.com/toh/GoogleImagesPostCard/Pesto-Egg-Salad-Sandwiches_exps89693_THHC2377559B01_09_3b_RMS.jpg'
+    },
+    {
+        recipeId:3,
+        categoryId:1,
+        title:'chicken sandwhich',
+        photo_url:
+        'https://i.ytimg.com/vi/5QoguRAgcvI/maxresdefault.jpg'
+    },
+    {
+        recipeId:4,
+        categoryId:2,
+        title:'cappuccino',
+        photo_url:
+        'https://i.ndtvimg.com/i/2017-02/coffee_620x318_41486141224.jpg'
+    }
+    {
+        recipeId:5,
+        categoryId:2,
+        title:'Dalgona coffee',
+        photo_url:
+        'https://food-images.files.bbci.co.uk/food/recipes/dalonga_coffee_20606_16x9.jpg'
+
+    },
+    {
+        recipe:6,
+        categoryId:2,
+        title:'Irish coffee',
+        photo_url:
+        'https://www.thespruceeats.com/thmb/9AvcAx8Y6zpDovld4tLVASqTl2Y=/3648x3648/smart/filters:no_upscale()/original-irish-coffee-recipe-759311-Hero-5b759d4b46e0fb005089915e.jpg'
+    }
+]
